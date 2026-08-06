0092f61eee9bddc142383956a248dc901770fbbb
Evan Purkhiser <evanpurkhiser@gmail.com>
2026-08-06 11:59:12 -0400

    Configure Cloudflare local development

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
+import {initOpenNextCloudflareForDev} from '@opennextjs/cloudflare';
 import type {NextConfig} from 'next';
 
+if (process.env.NODE_ENV === 'development') {
+  initOpenNextCloudflareForDev();
+}
+
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['100.124.157.144'],
   experimental: {
     useTypeScriptCli: true,
   },
